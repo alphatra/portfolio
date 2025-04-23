21cd05f79a153612d51fdca71b3ea08fa9ba40fd
gracjan <gracjan.ziemianski@bcf-software.com>
2025-04-23 04:04:50 +0200

    feat: Configure Vercel serverless adapter for API routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
+import vercel from "@astrojs/vercel/serverless";
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: vercel({}),
   integrations: [react()],
 
   vite: {
